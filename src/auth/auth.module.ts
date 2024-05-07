4bf5b9d22316b7eeeb76c207d2a505a42e8de202
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-07 15:55:36 -0500

    First commit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { jwtConstants } from './constants';
+import { SetMetadata } from '@nestjs/common';
+import { AuthGuard } from './auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
+  controllers: [AuthController],
+  exports: [AuthService],
+})
+export class AuthModule {}
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);

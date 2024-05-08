276dfd94c236f83d30e2dcc8aa6d8b77696994f7
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-08 10:31:23 -0500

    Auth guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -6,7 +6,6 @@ import {
     UnauthorizedException,
   } from '@nestjs/common';
   import { JwtService } from '@nestjs/jwt';
-  //import { jwtConstants } from './constants';
   import { Request } from 'express';
   import { Reflector } from '@nestjs/core';
 import { IS_PUBLIC_KEY } from './auth.module';
